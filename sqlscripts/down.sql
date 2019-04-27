cb26e3f37332e2eefec71a6f73a4b5f2ca1eeb0c
Taha-Firoz <42237761+Taha-Firoz@users.noreply.github.com>
2019-04-27 13:12:55 -0700

    Update down.sql

--- sqlscripts/down.sql
+++ sqlscripts/down.sql
@@ -1,41 +1,21 @@
-DROP TABLE ContentType;
-
-DROP TABLE MailType;
-
-DROP TABLE Priority;
-
-DROP TABLE Details;
-
-DROP TABLE StatusType;
-
-DROP TABLE POType;
-
-DROP TABLE Province;
-
-DROP TABLE Priority;
-
-DROP TABLE City;
-
-DROP TABLE PostOffice;
-
-DROP TABLE Employee;
-
-DROP TABLE MailDetail;
-
-DROP TABLE DomesticAddresses;
-
-DROP TABLE Customer;
-
-DROP TABLE Invoice;
-
-DROP TABLE Customer_Invoice;
-
-DROP TABLE Customer_Addresses;
-
-DROP TABLE Mail;
-
-DROP TABLE Mail_Invoice;
-
-DROP TABLE StatusTracking;
-    
 DROP TABLE PostalEmployee;
+DROP TABLE ContentType;
+DROP TABLE MailType;
+DROP TABLE Priority;
+DROP TABLE Details;
+DROP TABLE StatusType;
+DROP TABLE POType;
+DROP TABLE City;
+DROP TABLE Province;
+DROP TABLE Priority;
+DROP TABLE PostOffice;
+DROP TABLE Employee;
+DROP TABLE MailDetail;
+DROP TABLE DomesticAddresses;
+DROP TABLE Customer;
+DROP TABLE Invoice;
+DROP TABLE Customer_Invoice;
+DROP TABLE Customer_Addresses;
+DROP TABLE Mail;
+DROP TABLE Mail_Invoice;
+DROP TABLE StatusTracking;
