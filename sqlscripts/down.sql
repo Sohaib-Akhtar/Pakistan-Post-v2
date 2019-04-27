8e4bf6abdf848586b9aae92de35957e9acfa89ce
Taha-Firoz <42237761+Taha-Firoz@users.noreply.github.com>
2019-04-27 11:25:06 -0700

    Renamed a few files

---
+++ sqlscripts/down.sql
@@ -0,0 +1,41 @@
+DROP TABLE ContentType;
+
+DROP TABLE MailType;
+
+DROP TABLE Priority;
+
+DROP TABLE Details;
+
+DROP TABLE StatusType;
+
+DROP TABLE POType;
+
+DROP TABLE Province;
+
+DROP TABLE Priority;
+
+DROP TABLE City;
+
+DROP TABLE PostOffice;
+
+DROP TABLE Employee;
+
+DROP TABLE MailDetail;
+
+DROP TABLE DomesticAddresses;
+
+DROP TABLE Customer;
+
+DROP TABLE Invoice;
+
+DROP TABLE Customer_Invoice;
+
+DROP TABLE Customer_Addresses;
+
+DROP TABLE Mail;
+
+DROP TABLE Mail_Invoice;
+
+DROP TABLE StatusTracking;
+    
+DROP TABLE PostalEmployee;
