5aa88afb4f49e6d338ba87f6f8048154a74fd8c0
Sohaib-Akhtar <49755762+Sohaib-Akhtar@users.noreply.github.com>
2019-04-29 15:24:41 +0500

    sequence sql file

---
+++ sqlscripts/sequence.sql
@@ -0,0 +1,23 @@
+create sequence seq_details
+minvalue 300024
+start with 300024
+increment by 1
+cache 10;
+
+create sequence seq_address
+minvalue 102024
+start with 102024
+increment by 1
+cache 10;
+
+create sequence seq_barcode
+minvalue 40001933
+start with 40001933
+increment by 1
+cache 10;
+
+create sequence seq_invoice
+minvalue 230002938
+start with 230002938
+increment by 1
+cache 10;
