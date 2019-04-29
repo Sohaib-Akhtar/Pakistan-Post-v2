add6665c6534fe5ae9b31268a154c88913156cc5
Taha-Firoz <42237761+Taha-Firoz@users.noreply.github.com>
2019-04-29 05:27:03 -0700

    Removed Border

--- html/Invoice.php
+++ html/Invoice.php
@@ -3,17 +3,6 @@
 <html>
 <head>
 <style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
 </style>
 </head>
 <body>
